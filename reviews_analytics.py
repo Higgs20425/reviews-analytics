eee4bd4717b755c8b36175be0c475e36cd351bfd
frank <jack20425@gmail.com>
2021-04-07 07:04:37 +0800

    analytics for reviews

---
+++ reviews_analytics.py
@@ -0,0 +1,31 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			 print(len(data))
+# print(len(data))
+print('檔案讀完了,總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('每筆留言的平均長度為', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
+good = [d for d in data if 'good' in d] #List Comprehension	
+print('一共有', len(good), '筆留言有含good')
+
+bad = ['bad' in d for d in data]
+print(bad)
